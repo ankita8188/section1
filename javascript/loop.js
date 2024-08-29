ddfddeb5c642f8d1f9ab5607080b3f572574a9de
ankita8188 <at030038@gmail.com>
2024-08-29 11:05:27 +0530

    loops in javascript

---
+++ javascript/loop.js
@@ -0,0 +1,27 @@
+n=5
+for(let i=1;i<=10;i++)
+    console.log(n+" * "+i+" = "+n*i)
+
+console.log("-------------------------------------")
+
+for(let i=1;i<=10;i++)
+    console.log(`${n} * ${i} = ${n*i}`)
+
+fruits=[12,13,1,512,13]
+for(let j=0;j<fruits.length;j++)
+    console.log(fruits[j])
+
+dict={
+    Name:"ankita",
+    branch:"csd"
+}
+for(let key in dict)
+    console.log(key + " = " + dict[key])
+
+for(let k of fruits)
+    console.log(k)
+
+
+
+    
+
